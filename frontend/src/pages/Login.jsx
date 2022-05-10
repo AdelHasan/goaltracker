83fabe74774d0168fce18f521a60891955db9465
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-05-10 17:23:21 +0500

    completed register, login, logout

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,9 @@
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { login, reset } from "./../features/auth/authSlice";
+import Spinner from "./../components/Spinner";
 import { FaSignInAlt } from "react-icons/fa";
 
 function Login() {
@@ -9,14 +14,46 @@ function Login() {
 
   const { email, password } = formData;
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
-    }))
+    }));
   };
 
-  const onSubmit = (e) => {e.preventDefault()}
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      email,
+      password
+    }
+
+    dispatch(login(userData))
+  };
+
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
@@ -52,7 +89,9 @@ function Login() {
             />
           </div>
           <div className="form-group">
-            <button type="submit" className="btn btn-block">Submit</button>
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
           </div>
         </form>
       </section>
