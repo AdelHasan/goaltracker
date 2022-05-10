b9fdbf70df460f79860183903d4507bad237cdd1
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-05-10 11:38:22 +0500

    frontend UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,26 +1,28 @@
-import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { GiTargetShot } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className='header'>
+    <header className="header">
       <div className="logo">
-        <Link to='/'>GoalTrack</Link>
+        <GiTargetShot />
+        <Link to="/"> GoalTrack</Link>
       </div>
       <ul>
         <li>
-          <Link to='/login'>
+          <Link to="/login">
             <FaSignInAlt /> Login
           </Link>
         </li>
         <li>
-          <Link to='/register'>
+          <Link to="/register">
             <FaUser /> Register
           </Link>
         </li>
       </ul>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
