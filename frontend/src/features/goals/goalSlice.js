c926b03e5779464ace5675e5455eeb0011e46558
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-05-12 13:48:34 +0500

    goals slice in redux

---
+++ frontend/src/features/goals/goalSlice.js
@@ -0,0 +1,18 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  goals: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+export const goalSlice = createSlice({
+  name: "goal",
+  initialState,
+  reducers: (state) => initialState,
+});
+
+export const { reset } = goalSlice.actions;
+export default goalSlice.reducer
