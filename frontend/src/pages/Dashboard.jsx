958218ee1682b8ab5f9b49470c645f887a127d90
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-05-12 15:17:06 +0500

    fetch goals from server

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,25 +1,46 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import GoalForm from "../components/GoalForm";
+import Spinner from "./../components/Spinner";
+import { getGoals, reset } from "../features/goals/goalSlice";
 
 function Dashboard() {
-  const navigate = useNavigate()
-  const {user} = useSelector((state) => state.auth)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isError, message } = useSelector((state) => state.goals) 
 
   useEffect(() => {
-    if (!user) {
-      navigate('/login')
+    if (isError) {
+      console.log(message)
     }
-  }, [user, navigate])
-  
-  return <>
-    <section className="heading">
-      <h1>Hello, {user && user.name}</h1>
-      <p>Your Goals:</p>
-    </section>
-    <GoalForm />
-  </>;
+    
+    if (!user) {
+      navigate("/login");
+    }
+
+    dispatch(getGoals())
+
+    return () => {
+      dispatch(reset())
+    }
+  }, [user, navigate, isError, message, dispatch]);
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  return (
+    <>
+      <section className="heading">
+        <h1>Hello, {user && user.name}</h1>
+        <p>Your Goals:</p>
+      </section>
+      <GoalForm />
+    </>
+  );
 }
 
 export default Dashboard;
