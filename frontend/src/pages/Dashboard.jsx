ca83866ea765a440fc6da66afb28f640bb54d274
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-05-12 15:43:39 +0500

    Display goals in UI

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import GoalForm from "../components/GoalForm";
 import Spinner from "./../components/Spinner";
+import GoalItem from "../components/GoalItem";
 import { getGoals, reset } from "../features/goals/goalSlice";
 
 function Dashboard() {
@@ -39,6 +40,15 @@ function Dashboard() {
         <p>Your Goals:</p>
       </section>
       <GoalForm />
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (<h3>No Goals Set</h3>)}
+      </section>
     </>
   );
 }
