7cef2e9e3c14419f98ede826559fb4d0167a5357
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-05-12 14:53:28 +0500

    create goal extra reducer

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-API_URL = "/api/goals/";
+const API_URL = "/api/goals/";
 
 // Create new goal
 const createGoal = async (goalData, token) => {
@@ -10,7 +10,7 @@ const createGoal = async (goalData, token) => {
     },
   };
 
-  const response = await axios.post(API_URL, config, goalData);
+  const response = await axios.post(API_URL, goalData, config);
 
   return response.data;
 };
